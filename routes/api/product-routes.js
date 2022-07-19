9d7a47234c6a4798f82ec44d221f769934c2e1f4
Thomas Ruppeiner <truppeiner@gmail.com>
2022-07-19 13:32:31 -0500

    get product by ID added/tested

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -30,6 +30,26 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      },
+      {
+        model: Category,
+        attributes: ['category_name']
+      }
+    ]
+  })
+    .then(prodData => res.json(prodData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 // create new product
