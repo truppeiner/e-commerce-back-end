9365eacf30f8cafdcc10465778cc3380ddeff949
Thomas Ruppeiner <truppeiner@gmail.com>
2022-07-19 14:01:19 -0500

    delete TAG added/tested

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -91,6 +91,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy(
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then(tagData => {
+    res.json(tagData)
+    console.log('Tag successfully deleted')
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+    });
 });
 
 module.exports = router;
